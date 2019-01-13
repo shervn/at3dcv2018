6761ebc8521d3fa6f7f1e3d789026b2476039e1b
shervn <shervin.dehghani@tum.de>
2019-01-13 13:45:56 +0100

    do the ICP

--- src/Augmentation/augmentation.py
+++ src/Augmentation/augmentation.py
@@ -1,17 +1,19 @@
 import numpy as np
 from open3d import *
 import copy
+import sys,os
 
 from collections import defaultdict
 
-bedpclname = '../Samples/cropped.ply'
-bed_index = 13
+bed_pc_name = '/Samples/new_bed.ply'
+bed_pc_corners = [309941, 156052, 201237, 157628]
+bed_index = 15
+
+old_bed_corners = [1229, 2329, 240, 845]
 
 class Augmentor:
 
     def __init__(self, pointcloud, labeld_pointcloud):
-        print("Augmentor Just Starting...") 
-        print(bed_index)
         self.pointcloud = pointcloud
         self.labeld_pointcloud = labeld_pointcloud
         
@@ -20,15 +22,87 @@ class Augmentor:
 
         self.__find_all_objects()
 
-        #self.make_object_with_index(self.number_of_objects - 9)
+        # for i in range(0, self.number_of_objects):
+        #     print(self.number_of_objects - i)
+        #     draw_geometries([self.get_object_with_index(self.number_of_objects - i)])
 
-        self.remove_object_with_index(bed_index)
+    def get_object_with_index(self, i):
+         
+        new_pointcloud = select_down_sample(self.pointcloud, self.classfied_objects[i])
+        return new_pointcloud
+
+    def remove_object_with_index(self, i):
+
+        l = range(0, len(self.pointcloud.points))
+        new_pointcloud = select_down_sample(self.pointcloud, [x for x in l if x not in self.classfied_objects[i]])
+        return new_pointcloud
+    
+    def change_object(self, t):
+
+        scene_without_old_object = self.remove_object_with_index(bed_index)
+        old_object = self.get_object_with_index(bed_index)
+
+        transformed_new_object = self.__manual_registration(t, old_object)
+        #draw_geometries([transformed_new_object, scene_without_old_object])
+        draw_geometries([transformed_new_object, scene_without_old_object])
+        return transformed_new_object
+
+    def __manual_registration(self, t, target):
+
+        source = read_point_cloud(bed_pc_name) #ACCORDING TO t
+        picked_id_source = bed_pc_corners #ACCORDING TO t
+
+        #picked_id_target = self.__pick_points(target)
+        picked_id_target = old_bed_corners
+        assert(len(picked_id_source)>=3 and len(picked_id_target)>=3)
+        assert(len(picked_id_source) == len(picked_id_target))
+        corr = np.zeros((len(picked_id_source),2))
+        corr[:,0] = picked_id_source
+        corr[:,1] = picked_id_target
+
+        # estimate rough transformation using correspondences
+        p2p = TransformationEstimationPointToPoint()
+        trans_init = p2p.compute_transformation(source, target,
+                Vector2iVector(corr))
+
+        # point-to-point ICP for refinement
+        threshold = 0.03 # 3cm distance threshold
+        reg_p2p = registration_icp(source, target, threshold, trans_init,
+                TransformationEstimationPointToPoint())
+
+        source_temp = copy.deepcopy(source)
+        source_temp.transform(reg_p2p.transformation)
+        return (source_temp)
+
+    def __run_icp_for_objects(self, source, target, target_without_old_object):
+        
+        threshold = 0.02
+        trans_init = np.asarray(
+                    [[0.1, 0.1, -0.1,  0.1],
+                    [-0.1, 0.1, -0.1,  0.1],
+                    [0.1, 0.1,  0.1, -0.1],
+                    [0.1, 0.1, 0.1, 0.1]])
+                    
+        # trans_init = np.asarray(
+        #     [[0.0, 0.0, 0.0, 0.0],
+        #     [0.0, 0.0, 0.0, 0.0],
+        #     [0.0, 0.0, 0.0, 0.0],
+        #     [0.0, 0.0, 0.0, 0.0]])
+                    
+        evaluation = evaluate_registration(source, target,
+                threshold, trans_init)
+        reg_p2p = registration_icp(source, target, threshold, trans_init,
+                TransformationEstimationPointToPoint())
+
+        source.transform(reg_p2p.transformation)
+        
+        draw_geometries([source, target_without_old_object])
 
     def __find_all_objects(self):
         l = defaultdict(list)
         i = 0
         for color in self.labeld_pointcloud.colors:
-            l[self.get_object_index_by_label_color(color)].append(i)
+            l[self.ــget_object_index_by_label_color(color)].append(i)
             i += 1
 
         self.classfied_objects = []
@@ -38,16 +112,7 @@ class Augmentor:
 
         self.classfied_objects.sort(key = lambda x: len(x))
 
-    def get_object_with_index(self, i):
-        new_pointcloud = select_down_sample(self.pointcloud, self.classfied_objects[i])
-        return new_pointcloud
-
-    def remove_object_with_index(self, i):
-        l = range(0, len(self.pointcloud.points))
-        new_pointcloud = select_down_sample(self.pointcloud, [x for x in l if x not in self.classfied_objects[i]])
-        return new_pointcloud
-
-    def get_object_index_by_label_color(self, color):
+    def ــget_object_index_by_label_color(self, color):
         hashed_color = str(color[0]) + '-' + str(color[1]) + '-' + str(color[2])
         if(color is '0.0-0.0-0.0'):
             return -1
@@ -57,44 +122,27 @@ class Augmentor:
             self.color_hash_map[hashed_color] = self.number_of_objects
             self.number_of_objects += 1
             return self.color_hash_map[hashed_color]
-    
-    def change_object(self, t):
 
-        new_object = read_point_cloud(bedpclname) #ACCORDING TO t
-        scene_without_old_object = self.remove_object_with_index(bed_index)
-        old_object = self.get_object_with_index(bed_index)
-
-        # trans_init = np.asarray(
-        #     [[0.0, 0.0, 0.0, 0.0],
-        #     [0.0, 0.0, 0.0, 0.0],
-        #     [0.0, 0.0, 0.0, 0.0],
-        #     [0.0, 0.0, 0.0, 0.0]])
-
-        # draw_registration_result(old_object, new_object, trans_init, scene_without_old_object)
-        self.run_icp_for_objects(new_object, old_object, scene_without_old_object)
-
-    def run_icp_for_objects(self, source, target, target_without_old_object):
-        
-        threshold = 0.02
-        trans_init = np.asarray(
-                    [[0.862, 0.011, -0.507,  0.5],
-                    [-0.139, 0.967, -0.215,  0.7],
-                    [0.487, 0.255,  0.835, -1.4],
-                    [0.0, 0.0, 0.0, 1.0]])
-                    
-        evaluation = evaluate_registration(source, target,
-                threshold, trans_init)
-        reg_p2p = registration_icp(source, target, threshold, trans_init,
-                TransformationEstimationPointToPoint())
-
-        source.transform(reg_p2p.transformation)
-        draw_geometries([source, target_without_old_object])
+    def __pick_points(self, pcd):
+        print("")
+        print("1) Please pick at least three correspondences using [shift + left click]")
+        print("   Press [shift + right click] to undo point picking")
+        print("2) Afther picking points, press q for close the window")
+        vis = VisualizerWithEditing()
+        vis.create_window()
+        vis.add_geometry(pcd)
+        vis.run() # user picks points
+        vis.destroy_window()
+        print("")
+        return vis.get_picked_points()
 
 if __name__ == "__main__":
 
-    ll = read_point_cloud('../Samples/cropped.ply')
-    l = read_point_cloud('../Samples/scene0000_00_vh_clean_2.labels.ply')
-    r = read_point_cloud('../Samples/scene0000_00_vh_clean_2.ply')
+    rel_path = os.path.realpath('')
+    l = read_point_cloud(rel_path + '/Samples/scene0000_01_vh_clean_2.labels.ply')
+    r = read_point_cloud(rel_path + '/Samples/scene0000_01_vh_clean_2.ply')
+    bed_pc_name = rel_path + bed_pc_name
 
     t = Augmentor(r, l)
-    t.change_object(13)
+
+    t.change_object(bed_index)
