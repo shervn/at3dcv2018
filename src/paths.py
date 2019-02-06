209b0dd58f42c892baddcd49bb14a08ed9b50710
pti <vasl12@hotmail.com>
2019-02-06 11:45:40 +0100

    Layout + Images

--- src/paths.py
+++ src/paths.py
@@ -1,6 +1,8 @@
 import os
 
 rel_path = os.path.realpath('') + '/'
+index = len('src/')
+rel_path_before_src = os.path.realpath('')[0:-index]
 
 sensors = rel_path + 'Reconstruction/ReconstructionSystem/sensors'
 reconstruction_system = rel_path + 'Reconstruction/ReconstructionSystem/'
@@ -8,5 +10,19 @@ reconstruction_config = rel_path + 'Reconstruction/ReconstructionSystem/config/r
 utility = rel_path + 'Reconstruction/Utility'
 record_config = rel_path + 'Reconstruction/ReconstructionSystem/config/realsense.json'
 augmentation_util = rel_path + 'Augmentation/'
-macpyrealsense2 = os.path.realpath('') + '/pyrealsenseformac/'
 camera_config_path = rel_path + 'Reconstruction/ReconstructionSystem/config/realsense.json'
+
+
+reconstructed_scene = rel_path_before_src + '/Samples/scene0000_00_vh_clean_2.ply'
+segmented_reconstructed_scene = rel_path_before_src + '/Samples/scene0000_00_vh_clean_2.labels.ply'
+bed_pc_name = rel_path_before_src + '/Samples/new_bed_clean.ply'
+render_option_path = rel_path_before_src + '/Data/renderoption.json'
+camera_trajectory_path = rel_path_before_src + '/Data/camera_trajectory.log'
+
+macpyrealsense2 = rel_path_before_src + '/pyrealsenseformac/'
+
+furnitures_path = rel_path_before_src + '/Data/Furnitures/'
+logo_path = rel_path_before_src + '/logo.png'
+
+layout_path = rel_path_before_src + '/Layout'
+images_path = rel_path_before_src + '/Images'
